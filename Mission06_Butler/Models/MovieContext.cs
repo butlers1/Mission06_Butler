660f65ec30598bd253158f6361f9641f0b6576a4
butlers1 <butlers1@byu.edu>
2026-02-20 18:36:34 -0700

    Mission07

--- Mission06_Butler/Models/MovieContext.cs
+++ Mission06_Butler/Models/MovieContext.cs
@@ -9,14 +9,5 @@ namespace Mission06_Butler.Models
         }
 
         public DbSet<Movie> Movies { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder mb)
-        {
-            mb.Entity<Movie>().HasData(
-                new Movie { MovieId = 1, Category = "Action", Title = "The Dark Knight", Year = 2008, Director = "Christopher Nolan", Rating = "PG-13", Edited = false },
-                new Movie { MovieId = 2, Category = "Sci-Fi", Title = "Inception", Year = 2010, Director = "Christopher Nolan", Rating = "PG-13", Edited = false },
-                new Movie { MovieId = 3, Category = "Drama", Title = "The Prestige", Year = 2006, Director = "Christopher Nolan", Rating = "PG-13", Edited = false }
-            );
-        }
     }
 }
